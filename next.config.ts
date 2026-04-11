3221f5fac32459b6d6741a2351b41c598ab7107b
Maximilian Flack <maximilian.flack@outlook.de>
2026-04-12 01:55:49 +0200

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio",
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio",
   output: "export",  // <=== enables static exports
   images: {
     unoptimized: true,
