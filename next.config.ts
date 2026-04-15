146d6e630fd780c026b7c4200f4a72d9d1e1e129
Maximilian Flack <64138051+Maxi-F22@users.noreply.github.com>
2026-04-15 19:42:59 +0200

    Remove basePath and assetPrefix from config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/",
-  assetPrefix: "/",
+  basePath: "",
+  assetPrefix: "",
   output: "export",  // <=== enables static exports
   images: {
     unoptimized: true,
